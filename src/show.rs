d86d752e21ab2deeb2cfd81584818e283c8c357a
Nikos Filippakis <nikolaos.filippakis@cern.ch>
2019-10-17 20:13:17 +0200

    Still ugly but we got a ruler
    Signed-off-by: Nikos Filippakis <nikolaos.filippakis@cern.ch>

--- src/show.rs
+++ src/show.rs
@@ -1,44 +1,49 @@
 pub trait PresentNum {
     fn as_dec(&self) -> String;
     fn as_hex(&self) -> String;
-    fn as_bin(&self) -> String;
+    fn as_bin(&self) -> (String, String);
     fn show_all(&self);
-    fn group_str(s: String) -> String {
+    fn group_str(s: String, every: usize) -> (String, String) {
         let mut v: Vec<char> = Vec::new();
+        let mut ruler = String::from("----0 ");
         for (i, c) in s.chars().rev().enumerate() {
-            if i > 0 && i % 3 == 0 {
+            if i > 0 && i % every == 0 {
                 v.push(' ');
+                ruler.insert_str(0, format!("{0:->1$}", i, every + 1).as_str());
             }
             v.push(c);
         }
-        v.iter().rev().collect::<String>()
+        (v.iter().rev().collect::<String>(), ruler)
     }
 }
 
 impl PresentNum for i128 {
     fn as_dec(&self) -> String {
-        let mut s = Self::group_str(format!("{}", self));
+        let mut s = Self::group_str(format!("{}", self), 3).0;
         s.push_str("  ");
         s
     }
 
     fn as_hex(&self) -> String {
-        let mut s = Self::group_str(format!("{:x}", self));
+        let mut s = Self::group_str(format!("{:x}", self), 3).0;
         s.push_str(" h");
         s
     }
 
-    fn as_bin(&self) -> String {
-        let mut s = Self::group_str(format!("{:b}", self));
-        s.push_str(" b");
+    fn as_bin(&self) -> (String, String) {
+        let mut s = Self::group_str(format!("{:b}", self), 4);
+        s.0.push_str(" b");
         s
     }
 
     fn show_all(&self) {
-        let strs = vec![self.as_dec(), self.as_hex(), self.as_bin()];
+        let bin = self.as_bin();
+        let ruler = bin.1.chars().skip(bin.1.len() - bin.0.len()).collect::<String>();
+        let strs = vec![self.as_dec(), self.as_hex(), bin.0];
         let max_len = strs.iter().map(|s| s.len()).max().unwrap();
         for i in &strs {
             println!("{:>1$}", i, max_len);
         }
+        println!("{}", ruler);
     }
 }
